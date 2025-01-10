57448d0b92cfb7386edf6b676a38bd977bff96e7
Bantamlak <neoluka315@gmail.com>
2025-01-10 22:04:06 +0300

    first handlers setup

---
+++ internal/api/handlers/book_handler.go
@@ -0,0 +1,17 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func GetAllBooks(context *gin.Context) {}
+
+func GetBookById(context *gin.Context) {}
+
+func CreateBook(context *gin.Context) {}
+
+func UpdateBook(context *gin.Context) {}
+
+func DeleteBook(context *gin.Context) {}
+
+func UpdateBookStatus(context *gin.Context) {}
+
+func UpdateBookRating(context *gin.Context) {}
---
+++ internal/api/handlers/openlibrary_handler.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func SearchBooks(context *gin.Context) {}
+
+func GetBookByISBN(context *gin.Context) {}
