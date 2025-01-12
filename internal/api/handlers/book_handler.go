025611dfbbff898107a8512f2807ea6c2924f96e
Bantamlak <neoluka315@gmail.com>
2025-01-13 01:13:52 +0300

    making the handler dependent on the repository

--- internal/api/handlers/book_handler.go
+++ internal/api/handlers/book_handler.go
@@ -1,27 +1,41 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
-	"time"
 
 	"github.com/Bantamlak12/personal_book_library_manager/internal/models"
+	"github.com/Bantamlak12/personal_book_library_manager/internal/repository"
 	"github.com/gin-gonic/gin"
 )
+
+type Handler struct {
+	Repo *repository.SQLiteRepository
+}
+
+func NewHandler(repo *repository.SQLiteRepository) *Handler {
+	return &Handler{Repo: repo}
+}
 
 func GetAllBooks(context *gin.Context) {}
 
 func GetBookById(context *gin.Context) {}
 
-func CreateBook(context *gin.Context) {
+func (h *Handler) CreateBook(context *gin.Context) {
 	var data models.CreateBook
 	if err := context.ShouldBindJSON(&data); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	data.Id = "1"
-	data.CreatedAt = time.Now()
-	data.UpdatedAt = time.Now()
+	// Save the data
+	err := h.Repo.Create(&data)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the book: " + err.Error()})
+		return
+	}
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Book created!", "Book": data})
 }
 
