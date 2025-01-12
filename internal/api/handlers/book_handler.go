5f0696c0dbbf2dbf73fc753c5232d1ab2a0119b7
Bantamlak <neoluka315@gmail.com>
2025-01-12 21:59:59 +0300

    creating book logic on progress

--- internal/api/handlers/book_handler.go
+++ internal/api/handlers/book_handler.go
@@ -1,12 +1,29 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+
+	"github.com/Bantamlak12/personal_book_library_manager/internal/models"
+	"github.com/gin-gonic/gin"
+)
 
 func GetAllBooks(context *gin.Context) {}
 
 func GetBookById(context *gin.Context) {}
 
-func CreateBook(context *gin.Context) {}
+func CreateBook(context *gin.Context) {
+	var data models.CreateBook
+	if err := context.ShouldBindJSON(&data); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	data.Id = "1"
+	data.CreatedAt = time.Now()
+	data.UpdatedAt = time.Now()
+	context.JSON(http.StatusCreated, gin.H{"message": "Book created!", "Book": data})
+}
 
 func UpdateBook(context *gin.Context) {}
 
