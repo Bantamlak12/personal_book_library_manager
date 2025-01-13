c307f66d428017c48ff4f42c06246b8d7155365f
Bantamlak <neoluka315@gmail.com>
2025-01-13 21:30:49 +0300

    logic implemented to get all books with filtration support

--- internal/api/handlers/book_handler.go
+++ internal/api/handlers/book_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/Bantamlak12/personal_book_library_manager/internal/models"
 	"github.com/Bantamlak12/personal_book_library_manager/internal/service"
@@ -21,36 +22,63 @@ func NewBookHandler(bookService service.BookService) *BookHandler {
 	}
 }
 
-func GetAllBooks(context *gin.Context) {}
+type SuccessResponse struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
 
-func GetBookById(context *gin.Context) {}
-
-func (h *BookHandler) CreateBook(context *gin.Context) {
+func (h *BookHandler) CreateBook(c *gin.Context) {
 	var body models.CreateBook
-	if err := context.ShouldBindJSON(&body); err != nil {
-		utils.NewErrorResponse(context, http.StatusBadRequest, "INVALID_REQUEST", "Invalid input data", err.Error())
+	if err := c.ShouldBindJSON(&body); err != nil {
+		utils.NewErrorResponse(c, http.StatusBadRequest, "INVALID_REQUEST", "Invalid input data", err.Error())
 		return
-
 	}
 
 	// Save the body
 	data, err := h.bookService.CreateBK(&body)
 	if err != nil {
 		if errors.Is(err, service.ErrDuplicate) {
-			utils.NewErrorResponse(context, http.StatusConflict, "DUPLICATE_RESOURCE", "A book with this ISBN already exists", "")
+			utils.NewErrorResponse(c, http.StatusConflict, "DUPLICATE_RESOURCE", "A book with this ISBN already exists", "")
 		} else {
-			utils.NewErrorResponse(context, http.StatusInternalServerError, "INTERNAL_ERROR", "Failed to save the book", err.Error())
+			utils.NewErrorResponse(c, http.StatusInternalServerError, "INTERNAL_ERROR", "Failed to save the book", err.Error())
 		}
 		return
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"message": "Book created!", "Book": data})
+	response := SuccessResponse{
+		Status:  http.StatusOK,
+		Message: "Book created successfully",
+		Data:    data,
+	}
+	c.JSON(http.StatusCreated, response)
 }
 
-func UpdateBook(context *gin.Context) {}
+func (h *BookHandler) GetAllBooks(c *gin.Context) {
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "10"))
+	title := c.Query("title")
+	author := c.Query("author")
+	rating, _ := strconv.ParseFloat(c.Query("rating"), 64)
+	status := c.Query("status")
 
-func DeleteBook(context *gin.Context) {}
+	response, err := h.bookService.GetBooks(page, limit, rating, title, author, status)
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, "INTERNAL_ERROR", "Failed to get books", err.Error())
+		return
+	}
 
-func UpdateBookStatus(context *gin.Context) {}
+	c.JSON(http.StatusOK, response)
+}
 
-func UpdateBookRating(context *gin.Context) {}
+func (h *BookHandler) GetBookById(c *gin.Context) {}
+
+func (h *BookHandler) GetBookByISBN(context *gin.Context) {}
+
+func (h *BookHandler) UpdateBook(c *gin.Context) {}
+
+func (h *BookHandler) DeleteBook(c *gin.Context) {}
+
+func (h *BookHandler) UpdateBookStatus(c *gin.Context) {}
+
+func (h *BookHandler) UpdateBookRating(c *gin.Context) {}
--- internal/api/handlers/openlibrary_handler.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/gin-gonic/gin"
-
-func SearchBooks(context *gin.Context) {}
-
-func GetBookByISBN(context *gin.Context) {}
