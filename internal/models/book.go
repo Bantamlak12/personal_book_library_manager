0638a41e2ef7ccad2c999fe2ab642a90d6a03abd
Bantamlak <neoluka315@gmail.com>
2025-01-13 23:38:41 +0300

    unecessary struct type removed

--- internal/models/book.go
+++ internal/models/book.go
@@ -38,16 +38,6 @@ type CreateBook struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-type UpdateBook struct {
-	Title     string    `json:"title" binding:"required"`
-	Author    string    `json:"author" binding:"required"`
-	ISBN      string    `json:"isbn"`
-	Status    string    `json:"status" binding:"required,oneof=read unread"`
-	Rating    float64   `json:"rating" binding:"min=0,max=5"`
-	Notes     string    `json:"notes"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
-
 type UpdateStatus struct {
 	Status    string    `json:"status" binding:"required,oneof=read unread"`
 	UpdatedAt time.Time `json:"updated_at"`
