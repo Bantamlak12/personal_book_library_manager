6e2126be4b626593638f1487eeb289c08f996aee
Bantamlak <neoluka315@gmail.com>
2025-01-30 00:29:15 +0300

    limit changed to 12

--- backend/internal/api/handlers/book_handler.go
+++ backend/internal/api/handlers/book_handler.go
@@ -143,7 +143,7 @@ func (h *BookHandler) DeleteBook(c *gin.Context) {
 
 func (h *BookHandler) SearchBook(c *gin.Context) {
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
-	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "10"))
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "12"))
 	title := c.Query("title")
 	author := c.Query("author")
 	isbn := c.Query("isbn")
